e374ef1aa0af0884e6a8c3866db7c51143f7d1d0
Hugomka <335167@student.fontys.nl>
2021-03-09 17:42:41 +0100

    bingo-card component

---
+++ src/app/bingo-card/bingo-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {MockBingoCard} from '../mock-bingo-card';
+import {getNumber} from '../interfaces/bingo-row';
+import {faHome} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-bingo-card',
+  templateUrl: './bingo-card.component.html',
+  styleUrls: ['./bingo-card.component.css']
+})
+export class BingoCardComponent implements OnInit {
+
+  bingoCard = MockBingoCard;
+  drawNumber = '16';
+  faHome = faHome;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  get(numbers: string, index: number): string {
+    return getNumber(numbers, index);
+  }
+}
