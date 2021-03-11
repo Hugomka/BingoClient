c7c8d83ff8a9dbcb04c4c79fc433f3792b760eea
Hugomka <335167@student.fontys.nl>
2021-03-11 08:59:52 +0100

    Improved bingo mill

--- src/app/bingo-mill/bingo-mill.component.ts
+++ src/app/bingo-mill/bingo-mill.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {faHome, faPauseCircle, faPlayCircle} from '@fortawesome/free-solid-svg-icons';
 import {BingoMill} from '../interfaces/bingo-mill';
+import {BingoBallComponent} from '../bingo-ball/bingo-ball.component';
 import {CardType} from '../enums/card-type';
 
 @Component({
@@ -12,20 +13,22 @@ export class BingoMillComponent implements OnInit {
   faHome = faHome;
   bingoMill: BingoMill;
   participantsCounter = 0;
-  timerCounter = 15;
-  paused = false;
   faPlay = faPlayCircle;
   faPause = faPauseCircle;
-  drawnNumber: number;
   winner1: string;
   winner2: string;
   winner3: string;
   winnerFull: string;
 
-  drawnNumbers: number[] = [];
+  @ViewChild(BingoBallComponent)
+  bingoBallComponent: BingoBallComponent = new BingoBallComponent();
 
   constructor() {
-    this.bingoMill = { cardType: CardType.default, minimumNumber: 1, maximumNumber: 75};
+    this.bingoMill = {
+      cardType: CardType.default,
+      maximumNumber: 75,
+      minimumNumber: 1
+    };
   }
 
   ngOnInit(): void {
@@ -53,37 +56,11 @@ export class BingoMillComponent implements OnInit {
     return Array.from(Array(rows), (x, i) => i);
   }
 
-  drawNumber(): void {
-    const min = this.bingoMill.minimumNumber;
-    const max = this.bingoMill.maximumNumber + 1;
-    let justDraw;
-    while (true) {
-      justDraw = Math.floor(Math.random() * max + min);
-      if (this.drawnNumbers.length >= max) {
-        break;
-      }
-      if (!this.containNumber(justDraw)) {
-        break;
-      }
-    }
-    this.drawnNumbers.push(justDraw);
-    this.drawnNumber = justDraw;
-  }
-
-  containNumber(num: number): boolean {
-    for (const i of this.drawnNumbers) {
-      if (num === i) {
-        return true;
-      }
-    }
-    return false;
-  }
-
   startTimer(): void {
-    this.paused = false;
+    this.bingoBallComponent.paused = false;
   }
 
   pauseTimer(): void {
-    this.paused = true;
+    this.bingoBallComponent.paused = true;
   }
 }
